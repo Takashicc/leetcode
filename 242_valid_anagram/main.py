d200a563e636e364bd2cf369853bfa575e793315
Takashicc <105186894+Takashicc@users.noreply.github.com>
2022-10-05 23:02:25 +0900

    feat: 242 second solution

--- 242_valid_anagram/main.py
+++ 242_valid_anagram/main.py
@@ -9,29 +9,33 @@
 
 class Solution:
     # First solution
+    # def isAnagram(self, s: str, t: str) -> bool:
+    #     s_lookup = {}
+    #     for char in s:
+    #         if s_lookup.get(char) is None:
+    #             s_lookup[char] = 1
+    #         else:
+    #             s_lookup[char] += 1
+
+    #     t_lookup = {}
+    #     for char in t:
+    #         if t_lookup.get(char) is None:
+    #             t_lookup[char] = 1
+    #         else:
+    #             t_lookup[char] += 1
+
+    #     for key, value in s_lookup.items():
+    #         if t_lookup.get(key) is None:
+    #             return False
+
+    #         if t_lookup.get(key) != value:
+    #             return False
+
+    #     return len(s_lookup) == len(t_lookup)
+
+    # Second solution
     def isAnagram(self, s: str, t: str) -> bool:
-        s_lookup = {}
-        for char in s:
-            if s_lookup.get(char) is None:
-                s_lookup[char] = 1
-            else:
-                s_lookup[char] += 1
-
-        t_lookup = {}
-        for char in t:
-            if t_lookup.get(char) is None:
-                t_lookup[char] = 1
-            else:
-                t_lookup[char] += 1
-
-        for key, value in s_lookup.items():
-            if t_lookup.get(key) is None:
-                return False
-
-            if t_lookup.get(key) != value:
-                return False
-
-        return len(s_lookup) == len(t_lookup)
+        return sorted(s) == sorted(t)
 
 
 def main():
